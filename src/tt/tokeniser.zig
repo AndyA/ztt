d965a0ab625e6fc148b980fec39eb53bbf314003
Andy Armstrong <andy@hexten.net>
2025-11-23 18:18:37 +0000

    Minor

--- src/tt/tokeniser.zig
+++ src/tt/tokeniser.zig
@@ -14,6 +14,7 @@ pub const Keyword = enum {
             .@"!" => .NOT,
             .@"&&" => .AND,
             .@"<>" => .@"!=",
+            .@"=>" => .@"=",
             .@"|" => .FILTER,
             .@"||" => .OR,
             .FOR => .FOREACH,
